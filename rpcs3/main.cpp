f61ffa7ed8ba0f2f05ac4adbea0838297bf75c4b
Christian <christianaltermatt@gmail.com>
2020-09-25 09:47:46 -0600

    AAAAAAAAAAAA

--- rpcs3/main.cpp
+++ rpcs3/main.cpp
@@ -550,12 +550,13 @@ int main(int argc, char** argv)
 			game.bootable            = psf::get_integer(psf, "BOOTABLE", 0);
 			game.attr                = psf::get_integer(psf, "ATTRIBUTE", 0);
 			std::string serialnumber = game.serial;
-			std::string path         = dir + "/gameinfo.txt";
-			if(fs::file outputText{(path), fs::create})
-			{
-				sys_log.notice("Attempted txt file location: %s", (dir + "/gameinfo.txt"));
-				outputText.write("Game name: " + game.name + "\nGame serial: " + game.serial + "\nGame version: " + game.version);
-			}
+			fs::create_path(dir + "/gameinfo.txt");
+			fs::file outputText{(dir + "/gameinfo.txt")};
+			sys_log.notice("Attempted txt file location: %s", (dir + "/gameinfo.txt"));
+			outputText.write
+			(
+				"Game name: " + game.name + "\nGame serial: " + game.serial + "\nGame version: "+ game.version
+			);
 
 			return 0;
 		}else
